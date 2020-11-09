917a7be84317fe431ec3f0aee1932b9450a91973
inherited-checkpoint-3530 <webdevcarlosferreira@gmail.com>
2020-11-09 00:20:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var makeInt = function makeInt(string){
+  return parseInt(string,10);
+}
+makeInt();
+var preserveDecimal = function preserveDecimal(string){
+  return parseInt(string,10);
+}
+makeInt();
