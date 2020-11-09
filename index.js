a6c65d4b75df3bc65c87d35c715d5d252c22fc63
inherited-checkpoint-3530 <webdevcarlosferreira@gmail.com>
2020-11-09 00:23:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var increment = function increment(n){
+  n++;
+}
+increment();
+
+var decrement = function decrement(n){
+  n--;
+}
+decrement();
+
+
+
+
+
+
+
+
 var makeInt = function makeInt(string){
   return parseInt(string,10);
 }
