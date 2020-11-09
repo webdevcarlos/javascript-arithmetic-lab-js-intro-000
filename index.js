fe1608003a50401db6aca991cf4694a182d37b6e
inherited-checkpoint-3530 <webdevcarlosferreira@gmail.com>
2020-11-09 00:32:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ var multiply = function multiply(){
 }
 var divide = function divide(){
 }
+var number = 10
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number)
+
+add5()
+ 
+console.log(number) 
+
+
+
+
 
 var increment = function increment(n){
   return n += 1;
